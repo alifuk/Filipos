1f67f74c07fe12b7dd1db153189b8897f2e67af8
alifuk <amdalifuk@gmail.com>
2014-11-17 18:44:26 +0100

    Revert "edit"
    This reverts commit 87ed819bb27aff11dcfaa2900014a77d055a726a.

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
 	#pravyBlok{
 		width: 75%;
 		float: right;
-		background-color: #FFF;
+		background-color: #DAD;
 
 	}
 
@@ -64,22 +64,9 @@
 		}
 
 		.thumb{
-			max-width: 164px;
-			max-height: 133px;
-			padding: 12px;
-			position: absolute;
-    		top: 0;  
-    		bottom: 0;  
-    		left: 0;  
-    		right: 0; 
-    		margin:auto;
-		}
-
-		.thumbSpan{
-			width: 164px;
-			height: 133px;
-			display: inline-block;
-			position:relative;
+			width: 150px;
+			max-height: 150px;
+			padding: 5px;
 		}
 
 	}
@@ -135,7 +122,7 @@ foreach ($it as $fileinfo) {
 
 
             echo "
-            <span class=\"thumbSpan\">
+            <span class="">
                 <img class=\"thumb\" src=\" foto/voda/content/images/thumb/" . $fileinfo->getFilename() . " \" />
             </span>";
 
